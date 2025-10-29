23c20c2a8eba71cba10cc47e106eea4083dec6b1
mouwaficbdr <badaroumouwafic@gmail.com>
2025-10-29 19:23:14 +0100

    feat: ajout de la gestion des variantes de bouton et mise à jour des types

--- frontend/src/shared/Button/Button.tsx
+++ frontend/src/shared/Button/Button.tsx
@@ -1,15 +1,28 @@
 import React from "react";
-import type { ButtonProps } from "./Button.types";
+import type { ButtonProps, ButtonVariant } from "./Button.types";
 
 import clsx from "clsx"
 
-export const Button: React.FC<ButtonProps> = ({ children, disabled, className, ...rest }: ButtonProps) => {
+const variantStyle: Record<ButtonVariant, string> = {
+  primary: "bg-primary hover:bg-button-primary-hover text-neutral-900",
+  secondary: "bg-secondary hover:bg-button-secondary-hover text-white",
+  danger: "bg-red-600 hover:bg-red-700 text-white"
+} 
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  disabled,
+  className,
+  variant = "primary",
+  ...rest
+}: ButtonProps) => {
   const baseClasses =
-    'rounded-[2px] py-2 px-4 bg-primary hover:bg-button-primary-hover text-neutral-900 font-grotesk font-semibold transition-colors cursor-pointer';
+    'rounded-[2px] py-2 px-4 font-grotesk font-semibold transition-colors cursor-pointer';
 
   const finalClasses = clsx(
     baseClasses,
     className,
+    variantStyle[variant],
     {
     'opacity-50 pointer-events-none cursor-not-allowed': disabled,
     }
--- frontend/src/shared/Button/Button.types.ts
+++ frontend/src/shared/Button/Button.types.ts
@@ -1,7 +1,9 @@
 import { type ComponentPropsWithoutRef } from "react";
 
 export type ButtonHTML = ComponentPropsWithoutRef<'button'>
+export type ButtonVariant = "primary" | "secondary" | "danger"
 
 export interface ButtonProps extends ButtonHTML{
+  variant?: ButtonVariant
   children: React.ReactNode
 }
